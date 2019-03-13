5c154778daf19cb57e1d77e91a01be3f005d3cf0
sylvain.joube@gmail.com <40246406+SylvainLune@users.noreply.github.com>
2019-03-13 23:36:49 +0100

    Initialisation IndexTreeV4, idées

--- src/main/java/db/structure/indexTree/but_soutenance_intermediaire.java
+++ src/main/java/db/structure/indexTree/but_soutenance_intermediaire.java
@@ -9,6 +9,6 @@ package db.structure.indexTree;
 	- Un serveur central (aussi un noeud)
 	- Chargement des données par les noeuds, mise sur le disque pour pouvoir indexer beaucoup de données : les arbres sont mis sur disque lorsqu'il n'y a bientôt plus de mémoire dispo. Un index complet est composé d'un arbre en mémoire vive (arbre en cours) et d'une liste d'arbres sur disque (non modifiables, pour l'instant, plus tard modifiables), recherche sur un index complet : (point suivant)
 	- Lors d'une recherche, attribuer un thread par arbre sur disque (et le thread principal pour la recherche sur l'arbre en mémoire, éventuellement, ou un autre thread)
-	- Architecture multi-noeuds en réseau (NetBuffers, et TCPClient/Server)
+	- Architecture multi-noeuds en réseau : HTTP (comme Olivier le veut)  mais éventuellement, pourquoi pas une démo avec les (NetBuffers, et TCPClient/Server) ? (si j'ai le temps / du temps à perdre, Sylvain)
 	- Pour un index statique, on peut envisager d'avoir tout sur le disque, et de ne pas s'embêter avec un arbre en mémoire, également.
 */
