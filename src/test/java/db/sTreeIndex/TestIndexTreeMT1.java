8e3f891e1b5ae8eb08dcd6528ae3086468ac33a5
sylvain.joube@gmail.com <40246406+SylvainLune@users.noreply.github.com>
2019-03-16 14:09:04 +0100

    DateType modified, multi-thread still safe !

--- src/test/java/db/sTreeIndex/TestIndexTreeMT1.java
+++ src/test/java/db/sTreeIndex/TestIndexTreeMT1.java
@@ -43,7 +43,6 @@ public class TestIndexTreeMT1 {
 	
 	static ArrayList<Column> initializeColumnsForNYData() {
 		ArrayList<Column> columns = new ArrayList<Column>();
-		Utils currentlyUsedUils = new Utils(); // For thread-safety !
 		try {
 			columns.add(new Column("VendorID", new ByteType()));
 			columns.add(new Column("tpep_pickup_datetime", new DateType()));
@@ -129,8 +128,8 @@ public class TestIndexTreeMT1 {
 		
 		
 		for (int runCount = 0; runCount < maxRunCount; runCount++) {
-			threadList.get(runCount).start();   // 12,3 s
-			//runnableList.get(runCount).run(); // 25,7 s
+			threadList.get(runCount).start();   // 12,3 s (i3, Sylvain)
+			//runnableList.get(runCount).run(); // 25,7 s (i3, Sylvain)
 		}
 		for (int runCount = 0; runCount < maxRunCount; runCount++) {
 			threadList.get(runCount).join();
