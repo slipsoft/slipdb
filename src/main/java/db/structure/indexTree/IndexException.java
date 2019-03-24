c25efea53508ee5e1a723ad47d83f774034bbfce
sylvain.joube@gmail.com <40246406+SylvainLune@users.noreply.github.com>
2019-03-24 21:06:06 +0100

    IndexException, exceptions personnalisées (code pour ne pas avoir trop d'exceptions différentes)

--- src/main/java/db/structure/indexTree/IndexException.java
+++ src/main/java/db/structure/indexTree/IndexException.java
@@ -3,6 +3,10 @@ package db.structure.indexTree;
 public class IndexException extends Exception {
 	private static final long serialVersionUID = -8200666968152312521L;
 	
+	public final IndexExceptionCode exceptionCode;
 	
+	public IndexException(IndexExceptionCode argExceptionCode) {
+		exceptionCode = argExceptionCode;
+	}
 
 }
