f8a1c2d6d7f5c80ed7579aaee5f203400131f85b
sylvain.joube@gmail.com <40246406+SylvainLune@users.noreply.github.com>
2019-04-01 18:30:43 +0200

    Update MemUsage, change in TreeTests

--- src/main/java/com/dant/utils/MemUsage.java
+++ src/main/java/com/dant/utils/MemUsage.java
@@ -14,22 +14,38 @@ public class MemUsage {
 		printMemUsage(message, 4);
 	}
 	
-	public static void printMemUsage(String message, int logLevel) {
+	public static long getMemUsage() {
 		Runtime runtime = Runtime.getRuntime();
-
+		long allocatedMemory = runtime.totalMemory();
+		long freeMemory = runtime.freeMemory();
+		long usedMemory = allocatedMemory - freeMemory;
+		return usedMemory;
+	}
+	
+	public static String formatMemUsage(long usedMemory) {
 		NumberFormat format = NumberFormat.getInstance();
-
+		return format.format(usedMemory / 1024);
+	}
+	
+	public static void printMemUsage(String message, int logLevel) {
+		/*Runtime runtime = Runtime.getRuntime();
+		
+		
 		//StringBuilder sb = new StringBuilder();
 		//long maxMemory = runtime.maxMemory();
 		long allocatedMemory = runtime.totalMemory();
-		long freeMemory = runtime.freeMemory();
+		long freeMemory = runtime.freeMemory();*/
+		long usedMemory = getMemUsage();//allocatedMemory - freeMemory;
+		String formattedMemory = formatMemUsage(usedMemory);
+		
+		//NumberFormat format = NumberFormat.getInstance();
 		
 		
-		long usedMemory = allocatedMemory - freeMemory;
 		if (message == null) message = "";
 		if (message.equals("") == false) message += " ";
 		
-		Log.logInfoMessage(message + format.format(usedMemory / 1024), "MEMORY", 3);
+		//Log.logInfoMessage(message + format.format(usedMemory / 1024), "MEMORY", 3);
+		Log.logInfoMessage(message + formattedMemory, "MEMORY", 3);
 		
 		/*
 		Log.info("free memory: " + format.format(freeMemory / 1024));
