960089bc04fe7692c2e7a5bf69d8b312d9a74727
n-peugnet <n.peugnet@free.fr>
2019-06-02 02:29:11 +0200

    fix npe on viewEntity validation

--- src/main/java/com/dant/app/TableEndpoint.java
+++ src/main/java/com/dant/app/TableEndpoint.java
@@ -43,6 +43,7 @@ public class TableEndpoint {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response search(@PathParam("tableName") String tableName, ViewEntity view) {
+        view.setTableName(tableName);
         try {
             return Controller.doSearch(view);
 
