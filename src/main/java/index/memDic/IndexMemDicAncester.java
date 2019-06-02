3153003e7933b4f3f24009c76277ef9d4dd8dbf6
sylvain.joube@gmail.com <40246406+SylvainLune@users.noreply.github.com>
2019-06-02 23:07:22 +0200

    FIX IndexMemDic variables statiques mises en local (sauf une, pour le sort, tri pour la dichotomie)

--- src/main/java/index/memDic/IndexMemDicAncester.java
+++ src/main/java/index/memDic/IndexMemDicAncester.java
@@ -9,10 +9,16 @@ import db.structure.Table;
  */
 public class IndexMemDicAncester {
 	// Pensé pour être mono-thread, pour l'instant
-	public static Table table;
-	public static int[] colIndexArray; // dans l'ordre
-	public static Column[] indexOnThisColArray;
-
+	
+	protected Table table;
+	protected int[] colIndexArray; // dans l'ordre
+	protected Column[] indexOnThisColArray;
+	
+	//public static Table staticTable; désormais inutile
+	//public static int[] staticColIndexArray;
+	public static Column[] staticIndexOnThisColArray; // seulement utile pour le sort, utilisé depuis IndexMemDicTemporaryItem
+	// et actualisé avant tous les sort (dans IndexMemDic.refreshIndexWithColumnsData())
+	
 	public static boolean enableVerboseDichotomy = false;
 	public static boolean enableVerboseSort = false;
 	
